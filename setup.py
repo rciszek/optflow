4b671412b6d7329cd20d94d24b67ded9cda4c2db
rciszek <rkciszek@gmail.com>
2020-10-06 17:36:25 +0300

    Add functions to locate OpenCV without pkg-config. Add support for nvcc --compiler-bindir specification through CC.

--- setup.py
+++ setup.py
@@ -11,29 +11,87 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
 
+def find_path(file_regexp):
+    # Search for a path containing the file matching 'file_regexp' under /usr/
+    with subprocess.Popen(
+        ("find", "/usr/", "-name", file_regexp.split("/")[-1]),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ) as ps:
+        try:
+            grep_out = subprocess.check_output(
+                ["grep", "-P", "-o", "-m", "1", "^.*(?={})".format(file_regexp)],
+                stdin=ps.stdout,
+            )
+            ps.wait()
+            return grep_out.decode().strip()
+        except:
+            raise EnvironmentError(
+                "Could not locate {} under /usr/".format(file_regexp)
+            )
+
+
+def list_opencv_libs(lib_path):
+    # Construct a valid list of opencv libs
+    with subprocess.Popen(
+        ("find", lib_path), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
+    ) as ps:
+        try:
+            grep_out = subprocess.check_output(
+                ["grep", "-P", "-o", "(?<=/lib)(opencv.*)(?=.so)"], stdin=ps.stdout
+            )
+            ps.wait()
+            return ["-l" + l for l in grep_out.decode().splitlines()]
+        except:
+            raise EnvironmentError(
+                "Could not locate opencv libs under {}".format(lib_path)
+            )
+
+
 def locate_cuda():
     # first check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = os.path.join(home, 'bin', 'nvcc')
+    if "CUDAHOME" in os.environ:
+        home = os.environ["CUDAHOME"]
+        nvcc = os.path.join(home, "bin", "nvcc")
+        cuda_incs = home + "/include"
 
         if not os.path.exists(nvcc):
-            raise EnvironmentError('The CUDA path could not be located. CUDAHOME not properly set.')
+            raise EnvironmentError(
+                "The CUDA path could not be located. CUDAHOME not properly set."
+            )
     else:
         # otherwise, search for NVCC
         try:
-            nvcc = subprocess.check_output('which nvcc'.split()).decode()
+            nvcc = subprocess.check_output("which nvcc".split()).decode()
         except subprocess.CalledProcessError:
-            raise EnvironmentError('The CUDA path could not be located. Try to set CUDAHOME.')
+            raise EnvironmentError(
+                "The CUDA path could not be located. Try to set CUDAHOME."
+            )
 
-        home = os.path.abspath(os.path.dirname(nvcc) + '/..')
+        home = os.path.abspath(os.path.dirname(nvcc) + "/..")
 
-    cudaconfig = {'home': home, 'nvcc': nvcc}
+        cuda_incs = find_path("cuda.h")
+
+    cudaconfig = {"home": home, "nvcc": nvcc, "include": cuda_incs}
 
     return cudaconfig
 
 
+def check_gcc():
+    """User may define a specific gcc version if e.g system 
+    gcc version exceeds the maximum supported by CUDA"""
+    if "CC" in os.environ:
+        return os.environ["CC"]
+    else:
+        try:
+            # If CC is not set, use system gcc.
+            return subprocess.check_output("which gcc".split()).decode()
+        except subprocess.CalledProcessError:
+            raise EnvironmentError("The gcc path could not be located.")
+
+
 cuda = locate_cuda()
+cuda_gcc = check_gcc()
 
 
 def customize_compiler_for_nvcc(self):
@@ -46,7 +104,7 @@ def customize_compiler_for_nvcc(self):
     subclassing going on."""
 
     # tell the compiler it can processes .cu
-    self.src_extensions.append('.cu')
+    self.src_extensions.append(".cu")
 
     # save references to the default compiler_so and _comple methods
     default_compiler_so = self.compiler_so
@@ -57,14 +115,15 @@ def customize_compiler_for_nvcc(self):
     # based on source extension: we add it.
     def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
 
-        if os.path.splitext(src)[1] == '.cu':
+        if os.path.splitext(src)[1] == ".cu":
             # use the cuda for .cu files
-            self.set_executable('compiler_so', str(cuda['nvcc']))
+            self.set_executable("compiler_so", str(cuda["nvcc"]))
             # use only a subset of the extra_postargs, which are 1-1 translated
             # from the extra_compile_args in the Extension class
-            postargs = extra_postargs['nvcc']
+            postargs = extra_postargs["nvcc"]
+
         else:
-            postargs = extra_postargs['gcc']
+            postargs = extra_postargs["gcc"]
 
         super(obj, src, ext, cc_args, postargs, pp_opts)
         # reset the default compiler_so, which we might have changed for cuda
@@ -80,32 +139,47 @@ class custom_build_ext(build_ext):
         build_ext.build_extensions(self)
 
 
-opencv_libs_str = subprocess.check_output('pkg-config --libs opencv'.split()).decode()
-opencv_incs_str = subprocess.check_output('pkg-config --cflags opencv'.split()).decode()
+# Find opencv include dir, opencv lib path, and list opencv libs
+opencv_incs = [find_path("opencv2/core/core.hpp")]
+opencv_libs_path = find_path("libopencv_core.so")
+opencv_libs = list_opencv_libs(opencv_libs_path)
 
-opencv_libs = [str(lib) for lib in opencv_libs_str.strip().split()]
-opencv_incs = [str(inc) for inc in opencv_incs_str.strip().split()]
 
-eppm_src = ['EPPM/bao_pmflow_census_kernel.cu', 'EPPM/bao_pmflow_refine_kernel.cu',
-            'EPPM/bao_flow_patchmatch_multiscale_cuda.cpp',
-            'EPPM/bao_flow_patchmatch_multiscale_kernel.cu', 'EPPM/bao_pmflow_kernel.cu',
-            'EPPM/basic/bao_basic_cuda.cpp']
+eppm_src = [
+    "EPPM/bao_pmflow_census_kernel.cu",
+    "EPPM/bao_pmflow_refine_kernel.cu",
+    "EPPM/bao_flow_patchmatch_multiscale_cuda.cpp",
+    "EPPM/bao_flow_patchmatch_multiscale_kernel.cu",
+    "EPPM/bao_pmflow_kernel.cu",
+    "EPPM/basic/bao_basic_cuda.cpp",
+]
 
 extensions = [
-    Extension('optflow',
-              sources=['optflow.pyx'] + eppm_src,
-              include_dirs=[numpy.get_include(), cuda['home'] + '/include', 'EPPM', 'EPPM/basic'] + opencv_incs,
-              language='c++',
-              extra_link_args=opencv_libs + ['-lcudart', '-L' + cuda['home'] + '/lib', '-L' + cuda['home'] + '/lib64',
-                                             '-L' + cuda['home'] + '/lib/x86_64-linux-gnu', '-g'],
-              extra_compile_args={'gcc': ['-g'],
-                                  'nvcc': ['-arch=sm_30', '--ptxas-options=-v', '-c', '--compiler-options', "'-fPIC'"]},
-              )
+    Extension(
+        "optflow",
+        sources=["optflow.pyx"] + eppm_src,
+        include_dirs=[numpy.get_include(), cuda["include"], "EPPM", "EPPM/basic"]
+        + opencv_incs,
+        language="c++",
+        extra_link_args=["-L", opencv_libs_path] + opencv_libs + ["-lcudart", "-g"],
+        extra_compile_args={
+            "gcc": ["-g"],
+            "nvcc": [
+                "-arch=sm_30",
+                "--ptxas-options=-v",
+                "-c",
+                "--compiler-options",
+                "'-fPIC'",
+                "--compiler-bindir",
+                cuda_gcc,
+            ],
+        },
+    )
 ]
 
 setup(
-    name='optflow',
-    version='1.0',
+    name="optflow",
+    version="1.0",
     ext_modules=cythonize(extensions),
-    cmdclass={'build_ext': custom_build_ext},
+    cmdclass={"build_ext": custom_build_ext},
 )
